0c7574ddea03fe803fd82b115f2b935744a290d0
Jack Burrows <jackjb94@gmail.com>
2023-02-15 10:08:02 +0000

    added createSphereNode function

--- AR-App/ContentView.swift
+++ AR-App/ContentView.swift
@@ -108,80 +108,49 @@ class ARViewController: UIViewController, ARSCNViewDelegate {
     
     // TODO: First and last node are not part of the array, this needs fixing as they will not be destroyed
     override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
-            guard let touch = touches.first else { return }
-            // Create a raycast query from the touch location, allowing estimated planes and aligning with horizontal planes
-            let horizontalRaycastQuery = sceneView.raycastQuery(from: touch.location(in: sceneView), allowing: .estimatedPlane, alignment: .horizontal)!
-            let horizontalResults = sceneView.session.raycast(horizontalRaycastQuery)
+        guard let touch = touches.first else { return }
+        // Create a raycast query from the touch location, allowing estimated planes and aligning with horizontal planes
+        let horizontalRaycastQuery = sceneView.raycastQuery(from: touch.location(in: sceneView), allowing: .estimatedPlane, alignment: .horizontal)!
+        let horizontalResults = sceneView.session.raycast(horizontalRaycastQuery)
           
-            // Create a raycast query from the touch location, allowing estimated planes and aligning with horizontal planes
-            let verticalRaycastQuery = sceneView.raycastQuery(from: touch.location(in: sceneView), allowing: .estimatedPlane, alignment: .vertical)!
-            let verticalResults = sceneView.session.raycast(verticalRaycastQuery)
+        // Create a raycast query from the touch location, allowing estimated planes and aligning with horizontal planes
+        let verticalRaycastQuery = sceneView.raycastQuery(from: touch.location(in: sceneView), allowing: .estimatedPlane, alignment: .vertical)!
+        let verticalResults = sceneView.session.raycast(verticalRaycastQuery)
           
-            var hitResult: ARRaycastResult
+        var hitResult: ARRaycastResult
           
-            if let horizontalHitResult = horizontalResults.first {
-                hitResult = horizontalHitResult
-            } else if let verticalHitResult = verticalResults.first {
-                hitResult = verticalHitResult
-            } else {
-                return
-            }
-            // Extract the 3D position of the hit result
-            let position = SCNVector3(hitResult.worldTransform.columns.3.x, hitResult.worldTransform.columns.3.y, hitResult.worldTransform.columns.3.z)
-          
-            let color = UIColor.red
-            changeNodeColour(nodes, color: color)
-        
-            // Create a new sphere node at the touch position
-            let sphereNode = SCNNode()
-            sphereNode.geometry = SCNSphere(radius: 0.01)
-            sphereNode.position = position
-            // Add the sphere node to the scene
-            sceneView.scene.rootNode.addChildNode(sphereNode)
-            // If this is not the first point in the drawing, connect the current point with the previous point using a line
-            
-            if previousNode != nil {
-                sceneView.scene.rootNode.addChildNode(sphereNode)
-            }
-            // Update the previous node to be the current sphere node for the next point
-            nodes.append(sphereNode)
-            previousNode = sphereNode
+        if let horizontalHitResult = horizontalResults.first {
+            hitResult = horizontalHitResult
+        } else if let verticalHitResult = verticalResults.first {
+            hitResult = verticalHitResult
+        } else {
+            return
         }
-
-    // MARK: - createLine function
+        // Extract the 3D position of the hit result
+        let position = SCNVector3(hitResult.worldTransform.columns.3.x, hitResult.worldTransform.columns.3.y, hitResult.worldTransform.columns.3.z)
+          
+        // Create a new sphere node at the touch position
+        createSphereNode(at: position, nodes: &nodes, previousNode: &previousNode)
+    }
     
-    // This handles the functionality of creating lines from a series of sphere nodes
-    func createLine(from node1: SCNNode, to node2: SCNNode) -> [SCNNode] {
-        // Initialize an empty array to store the sphere nodes
-        var sphereNodes: [SCNNode] = []
-        // Calculate the distance between the two nodes
-        let distance = sqrt(
-          pow(node1.position.x - node2.position.x, 2) +
-          pow(node1.position.y - node2.position.y, 2) +
-          pow(node1.position.z - node2.position.z, 2)
-        )
-        // Determine the number of spheres to create based on the distance
-        let numSpheres = Int(distance / 0.01)
-        // Calculate the distance between each sphere
-        let sphereDistance = distance / Float(numSpheres)
-        // Create a vector to represent the direction of the line
-        let lineVector = SCNVector3(node2.position.x - node1.position.x, node2.position.y - node1.position.y, node2.position.z - node1.position.z)
-        // Iterate through the number of spheres to create
-        for i in 0..<numSpheres {
-          // Create a sphere node
-          let sphereNode = SCNNode()
-          // Set the sphere's geometry to a sphere with a radius of 0.005
-          sphereNode.geometry = SCNSphere(radius: 0.005)
-          // Calculate the position of the sphere based on the line vector and distance between spheres
-          let position = SCNVector3(node1.position.x + lineVector.x * (sphereDistance * Float(i)), node1.position.y + lineVector.y * (sphereDistance * Float(i)), node1.position.z + lineVector.z * (sphereDistance * Float(i)))
-          // Set the sphere node's position to the calculated position
-          sphereNode.position = position
-          // Append the sphere node to the array of sphere nodes
-          sphereNodes.append(sphereNode)
-          nodes.append(sphereNode)
-          }
-            // Return the array of sphere nodes
-            return sphereNodes
+    // This function creates the sphere nodes to allow the drawing within the app
+    func createSphereNode(at position: SCNVector3, nodes: inout [SCNNode], previousNode: inout SCNNode?) {
+        let color = UIColor.red
+        changeNodeColour(nodes, color: color)
+        // Create a new sphere node at the touch position
+        let sphereNode = SCNNode()
+        sphereNode.geometry = SCNSphere(radius: 0.01)
+        sphereNode.position = position
+        // Add the sphere node to the scene
+        sceneView.scene.rootNode.addChildNode(sphereNode)
+        // If this is not the first point in the drawing, connect the current point with the previous point using a line
+        
+        if previousNode != nil {
+            sceneView.scene.rootNode.addChildNode(sphereNode)
+        }
+        // Update the previous node to be the current sphere node for the next point
+        nodes.append(sphereNode)
+        previousNode = sphereNode
     }
     
     // TODO: Create a UI button to handle this function
