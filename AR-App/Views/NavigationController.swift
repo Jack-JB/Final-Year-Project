6f57d12e94ab8809dc93baaaea5a234af3db65e5
Jack Burrows <jackjb94@gmail.com>
2023-02-22 12:18:06 +0000

    Added navigation controller class

--- AR-App/Views/NavigationController.swift
+++ AR-App/Views/NavigationController.swift
@@ -6,3 +6,12 @@
 //
 
 import Foundation
+import SwiftUI
+
+class NavigationController: UINavigationController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+}
