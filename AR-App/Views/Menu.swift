33e839796cd1fe4101ae07cfd22fe03980d78cc3
Jack Burrows <jackjb94@gmail.com>
2023-03-01 15:24:39 +0000

    Added menu view

--- AR-App/Views/Menu.swift
+++ AR-App/Views/Menu.swift
@@ -6,15 +6,41 @@
 //
 
 import SwiftUI
+import Firebase
+import FirebaseCore
 
-struct Menu: View {
+struct Element: View {
+
+    var text: String
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        HStack {
+            Image(systemName: "scribble.variable")
+                .font(.system(size: 24))
+                .foregroundColor(.black)
+                .padding(.trailing, 10)
+            Text(text)
+        }
+    }
+}
+
+struct MenuView: View {
+
+    var body: some View {
+        NavigationView {
+            List{
+                Element(text: "Drawing 1")
+                Element(text: "Drawing 2")
+                Element(text: "Drawing 3")
+                Element(text: "Drawing 4")
+            }
+        }.navigationTitle("Drawing List")
     }
 }
 
 struct Menu_Previews: PreviewProvider {
     static var previews: some View {
-        Menu()
+        MenuView()
     }
 }
+
