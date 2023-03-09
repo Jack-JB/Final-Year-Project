71f4f58cb7bf1e0f3986b37cbe16c76c3b4cc812
Jack Burrows <jackjb94@gmail.com>
2023-03-09 15:21:45 +0000

    Refactor: Added data manager class, added utilities file

--- AR-App/Utilities/DataManager.swift
+++ AR-App/Utilities/DataManager.swift
@@ -6,3 +6,54 @@
 //
 
 import Foundation
+import SceneKit
+
+class DataManager {
+    var nodes = [SCNNode()]
+    
+    private func save() {
+        // Get the URL to the documents directory
+        let documentsDirectory = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first!
+        // Set the file name for the saved scene
+        let fileName = "Scene.arobject"
+        // Create the URL for the saved scene file
+        let fileURL = documentsDirectory.appendingPathComponent(fileName)
+        // Create an NSKeyedArchiver object to encode the nodes array
+        let archiver = NSKeyedArchiver(requiringSecureCoding: false)
+        // Encode the nodes array
+        archiver.encode(nodes, forKey: "nodes")
+        // Write the encoded data to the file
+        let data = archiver.encodedData
+        do {
+            try data.write(to: fileURL)
+        } catch {
+            print("Error saving scene: \(error)")
+        }
+    }
+
+    private func load() -> [SCNNode] {
+            // Get the URL to the documents directory
+            let documentsDirectory = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first!
+            // Set the file name for the saved scene
+            let fileName = "Scene.arobject"
+            // Create the URL for the saved scene file
+            let fileURL = documentsDirectory.appendingPathComponent(fileName)
+            // Initialize an NSKeyedUnarchiver object to decode the data from the file
+            do {
+                let data = try Data(contentsOf: fileURL)
+                let unarchiver = NSKeyedUnarchiver(forReadingWith: data)
+                // Decode the nodes array from the file
+                let decodedNodes = unarchiver.decodeObject(forKey: "nodes") as? [SCNNode]
+                // Check if the decoded nodes array is not nil
+                if let decodedNodes = decodedNodes {
+                    // Return the decoded nodes array
+                    return decodedNodes
+                } else {
+                    print("Error loading scene")
+                }
+            } catch {
+                print("Error reading data from file")
+            }
+            return []
+        }
+}
--- AR-App/Utilities/Utilities.swift
+++ AR-App/Utilities/Utilities.swift
@@ -6,3 +6,16 @@
 //
 
 import Foundation
+import SceneKit
+
+class Utilities {
+    // Debugging purposes: Check if 2 arrays are equal
+    private func areArraysEqual(_ array1: [SCNNode], _ array2: [SCNNode]) -> Bool {
+        return array1 == array2
+    }
+    
+    // Debugging purposes: Return the type of an array
+    private func getType<T>(_ array: [T]) -> String {
+        return "\(type(of: array))"
+    }
+}
