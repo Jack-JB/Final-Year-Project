c301b5bdec1e876922b448217a5a2d7824c6e68a
Jack Burrows <jackjb94@gmail.com>
2023-03-09 22:34:57 +0000

    Implemented testSaveNodesAsJSONFile

--- AR-AppTests/JsonManagerTests.swift
+++ AR-AppTests/JsonManagerTests.swift
@@ -105,6 +105,49 @@ class JsonManagerTests: XCTestCase {
         // Any test you write for XCTest can be annotated as throws and async.
         // Mark your test throws to produce an unexpected failure when your test encounters an uncaught error.
         // Mark your test async to allow awaiting for asynchronous code to complete. Check the results with assertions afterwards.
+    
+    func testSaveNodesAsJSONFile() {
+        // ARRANGE
+        // Create a test SCNNode
+        let testNode = SCNNode(geometry: SCNBox(width: 1, height: 1, length: 1, chamferRadius: 0))
+        testNode.name = "testNode"
+        testNode.position = SCNVector3(1, 2, 3)
+        
+        // Save the test node to a JSON file
+        let fileName = "testFile.json"
+        let saved = jsonManager.saveNodesAsJSONFile(nodes: [testNode], fileName: fileName)
+        XCTAssertTrue(saved, "Failed to save JSON file")
+        
+        // Read the contents of the saved JSON file
+        guard let documentsDirectory = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first else {
+            XCTFail("Error getting documents directory")
+            return
+        }
+        let fileURL = documentsDirectory.appendingPathComponent(fileName)
+        guard let jsonString = try? String(contentsOf: fileURL) else {
+            XCTFail("Error reading JSON file")
+            return
+        }
+        
+        // Convert the JSON string to a dictionary
+        guard let jsonDict = try? JSONSerialization.jsonObject(with: jsonString.data(using: .utf8)!, options: []) as? [String: Any] else {
+            XCTFail("Error parsing JSON string")
+            return
+        }
+        
+        // Assert that the dictionary contains the expected node data
+        guard let nodesArray = jsonDict["nodes"] as? [[String: Any]], let nodeData = nodesArray.first else {
+            XCTFail("Error getting nodes from JSON dictionary")
+            return
+        }
+        XCTAssertEqual(nodeData["name"] as? String, "testNode", "Node name is incorrect")
+        guard let positionData = nodeData["position"] as? [String: Any] else {
+            XCTFail("Error getting position data from JSON dictionary")
+            return
+        }
+        XCTAssertEqual(positionData["x"] as? Float, 1, "Node x position is incorrect")
+        XCTAssertEqual(positionData["y"] as? Float, 2, "Node y position is incorrect")
+        XCTAssertEqual(positionData["z"] as? Float, 3, "Node z position is incorrect")
     }
 
     func testPerformanceExample() throws {
