21124697d0c0f269cda2298b96d7377cd7f2865c
Jack Burrows <jackjb94@gmail.com>
2023-03-10 14:18:04 +0000

    Added code to load data from firebase

--- AR-App/Views/Menu.swift
+++ AR-App/Views/Menu.swift
@@ -8,6 +8,11 @@
 import SwiftUI
 import Firebase
 import FirebaseCore
+import ARKit
+
+protocol MenuDelegate: AnyObject {
+    func didSelectData(_ data: [String: Any])
+}
 
 struct DocumentRowView: View {
     let icon: Image
@@ -26,7 +31,9 @@ struct DocumentRowView: View {
 }
 
 struct MenuView: View {
-    
+    @State var jsonData: [String: Any] = [:]
+    @Environment(\.dismiss) var dismiss
+    weak var delegate: MenuDelegate?
     let firebaseManager = FirebaseManager()
     let arViewController = ARViewController()
     @State var documents: [DocumentSnapshot] = []
@@ -35,10 +42,11 @@ struct MenuView: View {
         NavigationView {
             List(documents, id: \.documentID) { document in
                 DocumentRowView(icon: Image(systemName: "scribble.variable"),
-                                documentID: document.documentID,
-                                onTap: {
-                                    arViewController.loadData(documentId: document.documentID)
-                                })
+                    documentID: document.documentID,
+                    onTap: {
+                        loadData(documentID: document.documentID)
+                        dismiss()
+                    })
             }
             .onAppear {
                 firebaseManager.loadData { result in
@@ -53,13 +61,34 @@ struct MenuView: View {
             .navigationTitle("Drawings")
         }
     }
+    
+    func loadData(documentID: String) {
+        let db = Firestore.firestore()
+        let collection = db.collection("myCollection")
+        let docId = documentID
+        
+        collection.document(docId).getDocument { (document, error) in
+            if let document = document, document.exists {
+                if let data = document.data() {
+                    // Successfully loaded the data
+                    JsonManager().saveJSONDataToFile(jsonData: data, fileName: "nodes.json")
+                    if JsonManager().checkJSONFileExists(fileName: "nodes.json") {
+                        print("File exists")
+                    }
+                }
+            } else {
+                print("Document does not exist")
+            }
+        }
+    }
 }
 
 
-
+/*
 struct Menu_Previews: PreviewProvider {
     static var previews: some View {
         MenuView()
     }
 }
 
+*/
