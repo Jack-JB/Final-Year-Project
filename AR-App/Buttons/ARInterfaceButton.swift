93cf3aee9d91e2177e4540adcef7d8fd4aeb0c77
Jack Burrows <jackjb94@gmail.com>
2023-03-13 11:23:57 +0000

    Updated button

--- AR-App/Buttons/ARInterfaceButton.swift
+++ AR-App/Buttons/ARInterfaceButton.swift
@@ -4,17 +4,33 @@
 //
 //  Created by Jack Burrows on 09/03/2023.
 //
+import UIKit
 
-import SwiftUI
-
-struct ARInterfaceButton: View {
-    var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+class ARInterfaceButton: UIButton {
+    var icon: String?
+    var titleText: String?
+    
+    convenience init(icon: String?, title: String?) {
+        self.init()
+        self.icon = icon
+        self.titleText = title
+        configureButton()
     }
-}
-
-struct ARInterfaceButton_Previews: PreviewProvider {
-    static var previews: some View {
-        ARInterfaceButton()
+    
+    private func configureButton() {
+        if let icon = icon {
+            setImage(UIImage(systemName: icon), for: .normal)
+            imageView?.contentMode = .scaleAspectFit
+            imageEdgeInsets = UIEdgeInsets(top: 0, left: -10, bottom: 0, right: 10) // Adjust icon position
+        }
+        if let titleText = titleText {
+            setTitle(titleText, for: .normal)
+            setTitleColor(.white, for: .normal)
+            titleLabel?.font = UIFont.systemFont(ofSize: 24, weight: .semibold)
+            contentEdgeInsets = UIEdgeInsets(top: 10, left: 20, bottom: 10, right: 20) // Adjust text position
+        }
+        backgroundColor = .blue
+        layer.cornerRadius = 10
+        translatesAutoresizingMaskIntoConstraints = false
     }
 }
